4392ff3975b7fd7197df67a184292155b4eeaf0f
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-21 17:32:51 +0000

    ajax GET request to the server to load all tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,49 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+var data = []
 
-$( document ).ready(function() {
 //creates emtpy tweet element and inserts data taken
 // from inputted tweet object into html skeleton
-  const createTweetElement = function (tweetObj) {
-    const name = tweetObj.user.name
-    const avatar = tweetObj.user.avatars.regular
-    const handle = tweetObj.user.handle
-    const text = (tweetObj.content.text).toString()
-    const created = (tweetObj.created_at/6000)
-    const tweetSkeleton = $(`<article class="tweet">
-    <header>
-    <img class="avatar" src="${avatar}">
-    <h1> ${name} </h1>
-    <h5> ${handle} </h5>
-    </header>
-    <p> ${text} </p>
-    <footer>
-    <p> ${created} </p>
-    <i class="fa fa-heart"></i>
-    <i class="fa fa-retweet"></i>
-    <i class="fa fa-flag"></i>
-    </footer>
-    </article>`)
-    $('#tweets').prepend(tweetSkeleton)
-  }
+const createTweetElement = function (tweetObj) {
+  const name = tweetObj.user.name
+  const avatar = tweetObj.user.avatars.regular
+  const handle = tweetObj.user.handle
+  const text = (tweetObj.content.text).toString()
+  const created = (tweetObj.created_at/6000)
+  const tweetSkeleton = $(`<article class="tweet">
+  <header>
+  <img class="avatar" src="${avatar}">
+  <h1> ${name} </h1>
+  <h5> ${handle} </h5>
+  </header>
+  <p> ${text} </p>
+  <footer>
+  <p> ${created} </p>
+  <i class="fa fa-heart"></i>
+  <i class="fa fa-retweet"></i>
+  <i class="fa fa-flag"></i>
+  </footer>
+  </article>`)
+  $('#tweets').prepend(tweetSkeleton)
+}
+
 //for runs createTweetElement function  for each
 //object (index) in the array given
-  const renderTweets = function (data) {
-    data.forEach(function(tweetObj) {
-      var $tweet = createTweetElement(tweetObj)
-      $('#tweets').append($tweet)
-    }
-  )}
+const renderTweets = function (data) {
+  data.forEach(function(tweetObj) {
+    var $tweet = createTweetElement(tweetObj)
+    $('#tweets').append($tweet)
+  }
+)}
+
+const loadTweets = function(){
+  //make a ajax request to /tweets
+  $.get('/tweets', {}, renderTweets)
+    .fail(function(error) {
+      console.error(error)
+    })
+}
+
+$( document ).ready(function() {
+  loadTweets()
 
 //prevents default post request on submit button
 //submits an async ajax request instead
@@ -46,11 +53,17 @@ $( document ).ready(function() {
     let newTweet = $(this).serialize();
     //posting via ajax
     $.post('/tweets/', newTweet, function(){
-      debugger
+      .done(function(result) {
+      loadTweets()
+      })
+      .fail(function(error) {
+      console.error(error)
+      })
     })
     //on success reder all the tweets again
     // on failure error message
   })
+
 
 //these are the closing brackets for $(doc).ready
 })
@@ -62,49 +75,3 @@ $( document ).ready(function() {
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
